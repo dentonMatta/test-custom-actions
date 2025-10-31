49596d181d815832a1dde21bc053187cf0b55020
Femi Denton <femidenton@yahoo.co.uk>
2025-10-31 18:50:24 +0100

    should work

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,7 +3,7 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run(){
-    core.notice('Starting deployment to S3 bucket');
+    core.notice('Hello from Deploy to S3 JavaScript Action!');
 }
 
 run();
